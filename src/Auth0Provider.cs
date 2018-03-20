b5cc64988f9bd45b1128db9e5c3b9e14b7046151
Rong Kang <chewrongkang@gmail.com>
2018-03-20 15:39:23 +0800

    Added a dev flag to bypass auth0 during dev

--- src/Auth0Provider.cs
+++ src/Auth0Provider.cs
@@ -14,32 +14,40 @@ namespace Auth0AutoRest
         private readonly Auth0TokenRequest _auth0TokenRequest;
         private Auth0TokenResponse _cachedToken;
         private DateTime _expiry;
+        private bool _isAuthRequired;
 
-        public Auth0Provider(Auth0TokenRequest auth0TokenRequest) : this(auth0TokenRequest, TimeSpan.FromMinutes(10))
+        public Auth0Provider(Auth0TokenRequest auth0TokenRequest, bool isAuthRequired = true) : this(auth0TokenRequest, TimeSpan.FromMinutes(10), isAuthRequired)
         {
         }
 
-        public Auth0Provider(Auth0TokenRequest auth0TokenRequest, TimeSpan tokenTimeout)
+        public Auth0Provider(Auth0TokenRequest auth0TokenRequest, TimeSpan tokenTimeout, bool isAuthRequired = true)
         {
             TokenTimeout = tokenTimeout;
             _auth0TokenRequest = auth0TokenRequest;
+            _isAuthRequired = isAuthRequired;
         }
 
         public async Task<AuthenticationHeaderValue> GetAuthenticationHeaderAsync(CancellationToken cancellationToken)
         {
-            if (_cachedToken == null || DateTime.UtcNow + TokenTimeout > _expiry)
+            if (_isAuthRequired)
             {
-                var tokenResponse = await _auth0TokenRequest.Auth0Endpoint
-                    .PostJsonAsync(_auth0TokenRequest)
-                    .ReceiveJson<Auth0TokenResponse>();
+                if (_cachedToken == null || DateTime.UtcNow + TokenTimeout > _expiry)
+                {
+                    var tokenResponse = await _auth0TokenRequest.Auth0Endpoint
+                        .PostJsonAsync(_auth0TokenRequest)
+                        .ReceiveJson<Auth0TokenResponse>();
 
-                var exp = new JwtSecurityTokenHandler().ReadJwtToken(tokenResponse.AccessToken).Payload.Exp;
-                var offset = exp == null ? 0 : (long) exp;
-                _expiry = DateTimeOffset.FromUnixTimeSeconds(offset).UtcDateTime;
-                _cachedToken = tokenResponse;
+                    var exp = new JwtSecurityTokenHandler().ReadJwtToken(tokenResponse.AccessToken).Payload.Exp;
+                    var offset = exp == null ? 0 : (long) exp;
+                    _expiry = DateTimeOffset.FromUnixTimeSeconds(offset).UtcDateTime;
+                    _cachedToken = tokenResponse;
+                }
+                return _cachedToken.AuthenticationHeader;
             }
-
-            return _cachedToken.AuthenticationHeader;
+            else
+            {
+                return new AuthenticationHeaderValue("Bearer", "Not getting token from Auth0 when isAuthRequired=false (env=Development)");
+            }
         }
     }
 }
